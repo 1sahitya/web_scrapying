aae9dd291d4682ceb9e72b49462a9151356be9d9
11sahitya@gmail.com <11sahitya@gmail.com>
2019-05-15 17:01:18 +0530

    first commit

---
+++ sahi.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = 'https://www.newegg.com/DailyDeal.aspx?name=DailyDeal&cm_sp=Homepage_Dailydeal-_--_-05152019'
+
+
+#  opnening up connection , grabbing the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+#HTML PARSING
+ page_soup = soup(page_html, "html.parser")
+
+ containers = page_soup.findAll("div" , {"class": "item-container"})
